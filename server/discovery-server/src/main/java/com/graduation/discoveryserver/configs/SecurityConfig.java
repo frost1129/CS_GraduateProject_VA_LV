7a181ed1974c55083b7183bc5fb62e2c126b64c8
Anh Nguyen Van <anhnv.forwork@gmail.com>
2024-03-14 09:08:39 +0700

    feat: config api gateway & discovery server

---
+++ server/discovery-server/src/main/java/com/graduation/discoveryserver/configs/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.graduation.discoveryserver.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/eureka/**").permitAll();
+                });
+        return httpSecurity.build();
+    }
+}
