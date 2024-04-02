0e356880712118c5324a23f16524637b1dbd69dd
Rinv <prodige112002@gmail.com>
2024-04-02 20:20:27 +0700

    [Linh] refactor docker-compose for initialize database

--- server/keycloak-auth-service/current_db/keycloak_db.sql
+++ server/keycloak-auth-service/current_db/keycloak_db.sql
@@ -1,6 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `keycloak_auth_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `keycloak_auth_service_db`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: keycloak_auth_service_db
+-- Host: 127.0.0.1    Database: keycloak_auth_service_db
 -- ------------------------------------------------------
 -- Server version	8.0.36
 
@@ -2688,4 +2690,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-01 12:07:43
+-- Dump completed on 2024-04-02 19:48:07
--- server/keycloak-auth-service/db_backup_protocol.md
+++ server/keycloak-auth-service/db_backup_protocol.md
@@ -2,7 +2,7 @@
 - Step 1: Use MYSQL Workbench to connect to the current database.
 - Step 2: Select 'Server' > 'Data Export'
 - Step 3: Specify 'Tables to Export'
-- Step 4: At 'Export Options', choose 'Export to Self-Contained File'
+- Step 4: At 'Export Options', choose 'Export to Self-Contained File' and 'Include Create Schema'
 
 
 ## 2. WHERE TO STORE BACKUP FILES
@@ -12,3 +12,6 @@
 ## 3. WHAT IS 'current_db' FOLDER FOR
 - This folder store **ONLY 1** backup file to use to init database/schema on new MySQL Container creation. 
 - The stored file can be stored with any name (doesn't have to be the same as the database).
+
+
+## 4. REMEMBER TO CHECK THE `Include Create Schema` OPTION WHEN EXPORT DATABASE/SCHEMA
