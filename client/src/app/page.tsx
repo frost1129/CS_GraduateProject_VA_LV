3b8338b8def7703d171c7668ee0e20abd426038d
Anh Nguyen Van <anhnv.forwork@gmail.com>
2024-04-09 13:42:20 +0700

    style: dump change

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Button, Stack, Typography } from "@mui/material"
+import { Typography } from "@mui/material"
 
 const HomePage = () => {
   return (
-    <Stack direction='column' gap={1} width='fit-content' padding={2}>
-      <Button variant="contained" color="tertiary" size="medium">Click</Button>
-      <Button variant="outlined" color="secondary">Click</Button>
-      <Button variant="text" color="primary">Click</Button>
-    </Stack>
+    <Typography>HomePage</Typography>
   )
 }
 
