2be31216f23302523d093e61363e34f96195ce90
Linh Vũ <69843581+frost1129@users.noreply.github.com>
2024-05-03 09:19:47 +0700

    [Linh] BE: update schedule-service apis

--- server/schedule-service/src/main/java/com/graduation/scheduleservice/controllers/StudentJoinClassController.java
+++ server/schedule-service/src/main/java/com/graduation/scheduleservice/controllers/StudentJoinClassController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.Console;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
--- server/schedule-service/src/main/java/com/graduation/scheduleservice/repositories/SubjectClassRepository.java
+++ server/schedule-service/src/main/java/com/graduation/scheduleservice/repositories/SubjectClassRepository.java
@@ -2,8 +2,6 @@ package com.graduation.scheduleservice.repositories;
 
 import com.graduation.scheduleservice.models.SubjectClass;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
