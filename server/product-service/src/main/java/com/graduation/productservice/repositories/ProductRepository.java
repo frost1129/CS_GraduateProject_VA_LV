3d4985cc709ace507091322b6595ce203a4ae730
Anh Nguyen Van <anhnv.forwork@gmail.com>
2024-05-05 09:57:41 +0700

    refactor: remove product service

--- server/product-service/src/main/java/com/graduation/productservice/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.graduation.productservice.repositories;
-
-import com.graduation.productservice.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
