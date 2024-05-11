087254fb12c5848acf66862eba58c51d1b3a9c55
Linh Vũ <69843581+frost1129@users.noreply.github.com>
2024-05-11 23:08:06 +0700

    [Linh] update scheduling api

--- server/schedule-service/src/main/java/com/graduation/scheduleservice/constants/FitnessPenalty.java
+++ server/schedule-service/src/main/java/com/graduation/scheduleservice/constants/FitnessPenalty.java
@@ -1,7 +1,7 @@
 package com.graduation.scheduleservice.constants;
 
 public class FitnessPenalty {
-    public static final int FIRST_CLASS = 100;
+    public static final int FIRST_CLASS = 1000;
     public static final int SECOND_CLASS = 5;
     public static final int THIRD_CLASS = 1;
 }
